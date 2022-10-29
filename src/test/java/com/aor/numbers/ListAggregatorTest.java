1efecbce27ae370c45421a634734040813263fa6
acarolinacc <acarolinacc2003@gmail.com>
2022-10-29 18:33:14 +0100

    Created helpers for testers

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -1,39 +1,42 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListAggregatorTest {
+
+    private List<Integer> list = Arrays.asList(1,2,4,2,5);
+
+    @BeforeEach
+    private void helper() {
+        list = Arrays.asList(1,2,4,2,5);
+    }
+
+
+
     @Test
     public void sum() {
         List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
-
         Assertions.assertEquals(14, sum);
     }
-
     @Test
     public void max() {
         List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
-
         Assertions.assertEquals(5, max);
     }
-
     @Test
     public void min() {
         List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int min = aggregator.min(list);
-
         Assertions.assertEquals(1, min);
     }
 
@@ -43,7 +46,6 @@ public class ListAggregatorTest {
 
         ListAggregator aggregator = new ListAggregator();
         int distinct = aggregator.distinct(list);
-
         Assertions.assertEquals(4, distinct);
     }
 }
--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -1,6 +1,7 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,6 +9,13 @@ import java.util.List;
 
 public class ListDeduplicatorTest {
 
+    private List<Integer> list;
+    private List<Integer> expected;
+
+    @BeforeEach void helper() {
+        list = Arrays.asList(1,2,4,2,5);
+        expected = Arrays.asList(1,2,4,5);
+    }
     @Test
     public void deduplicate() {
         List<Integer> list = Arrays.asList(1,2,4,2,5);
