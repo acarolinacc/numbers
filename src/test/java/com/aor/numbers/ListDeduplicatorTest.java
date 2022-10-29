c9b4b74e46e69f1e75446432980b1c3cd932830e
acarolinacc <acarolinacc2003@gmail.com>
2022-10-29 18:52:17 +0100

    Added stubs and tried dependy injection. Solved sorting bug.

--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -2,7 +2,7 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import jdk.internal.org.jline.terminal.impl.LineDisciplineTerminal;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import java.util.Arrays;
 import java.util.List;
@@ -16,7 +16,13 @@ public class ListDeduplicatorTest {
     }
     @Test
     public void deduplicate() {
-        ListDeduplicator deduplicator = new ListDeduplicator();
+        class StubListSorter implements GenericListSorter {
+            @Override public List<Integer> sort(List<Integer> list){
+                return Arrays.asList(1,2,4,5);
+            }
+        }
+        StubListSorter sorter = new StubListSorter();
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
         List<Integer> distinct = deduplicator.deduplicate(list);
 
         Assertions.assertEquals(expected, distinct);
@@ -26,7 +32,13 @@ public class ListDeduplicatorTest {
     public void bug_8726() {
         list = Arrays.asList(1,2,4,2);
 
-        ListDeduplicator dedup = new ListDeduplicator();
+        class StubListSorter implements GenericListSorter{
+            @Override public List<Integer> sort(List<Integer> list) {
+                return Arrays.asList(1,2,2,4);
+            }
+        }
+        StubListSorter sorter = new StubListSorter();
+        ListDeduplicator dedup = new ListDeduplicator(sorter);
         List<Integer> ret = dedup.deduplicate(list);
 
         Assertions.assertEquals(Arrays.asList(1,2,4),ret);
--- src/test/java/com/aor/numbers/ListSorterTest.java
+++ src/test/java/com/aor/numbers/ListSorterTest.java
@@ -16,6 +16,15 @@ public class ListSorterTest {
         expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
     }
     @Test
+    public void bug_8726() {
+        list = Arrays.asList(1,2,4,2);
+
+        ListSorter sorter = new ListSorter();
+        List<Integer> sorted = sorter.sort(list);
+
+        Assertions.assertEquals(Arrays.asList(1,2,2,4),sorted);
+    }
+    @Test
     public void sort() {
         List<Integer> list = Arrays.asList(3, 2, 6, 1, 4, 5, 7);
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
